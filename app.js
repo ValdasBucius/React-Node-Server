7a0d26b803790a15105df51b18808c3b9153c95e
Nestinio <valdasbucius1@gmail.com>
2024-07-08 22:50:06 -0700

    Before refactoring

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.json());
 
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
@@ -15,7 +16,72 @@ const getUsers = app.get('/api/v1/users', (req, res) => {
   });
 });
 
-const port = 9000;
-app.listen(port, () => {
-  console.log('Server is listening');
+const createUser = app.post('/api/v1/users', (req, res) => {
+  const newId = users[users.length - 1].id + 1;
+  const newUser = Object.assign({ id: newId }, req.body);
+  users.push(newUser);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/users.json`,
+    JSON.stringify(users),
+    (err) => {
+      res.status(201).json({
+        status: 'successfull',
+        data: {
+          users: newUser,
+        },
+      });
+    }
+  );
 });
+
+const getUser = app.get('/api/v1/users/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tour.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const user = users.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+const updateUser = app.patch('/api/v1/users/:id', (req, res) => {
+  if (req.params.id > users.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: `There's no such id`,
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: '<Updated tour here>',
+    },
+  });
+});
+
+const deleteUser = app.delete('/api/v1/users/:id', (req, res) => {
+  if (req.params.id > users.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: `There's no such id`,
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+module.exports = app;
